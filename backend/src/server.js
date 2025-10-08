ddfd570445e0d9e269530e8f22f66a1d4886070f
ArafiFnya2 <raffiluthfiansyah@gmail.com>
2025-10-08 11:42:29 +0700

    dasdasd

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,12 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: 'https://kodein-notes-omega.vercel.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
