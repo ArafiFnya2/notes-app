ddd01ce686c55a7e4055d568689588d47eae4837
ArafiFnya2 <raffiluthfiansyah@gmail.com>
2025-10-08 12:41:14 +0700

    adsa

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,20 @@ import noteRouter from "./routes/notesRoute.js";
 import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: ["https://kodein-notes-omega.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use(express.json());
-app.use(cors());
 
-
-
-
-const port = 3000;
 app.use("/", helloRouter);
 app.use("/notes", noteRouter);
 
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-    testConnection();
+  console.log(`Server running at http://localhost:${port}`);
+  testConnection();
 });
